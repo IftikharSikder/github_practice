fc31f0ce9432b5787e9dceca673f1268b991ed88
Iftikhar <eftyshikder111@gmail.com>
2024-11-11 21:56:29 +0600

    Deleted Unnecessary folder

--- lib/appbar.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-appBarStyle() {
-  return AppBar(
-    backgroundColor: Colors.teal,
-    centerTitle: true,
-    title: const Text("Headspace",style: TextStyle(fontWeight: FontWeight.bold),),
-  );
-}
